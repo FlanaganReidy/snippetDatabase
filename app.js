8e67f1717b3d5db066f7d7cf7416b3a3885bf4da
Shannon <reidy.pf@gmail.com>
2017-08-30 17:46:35 -0500

    CRU version 0.1

--- app.js
+++ app.js
@@ -14,63 +14,92 @@ const ObjectId = require('mongodb').ObjectID;
 
 let app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use('/public',express.static(path.join(__dirname, '/public')));
-  app.engine('mustache', mustacheExpress());
-  app.set('views', './views');
-  app.set('view engine', 'mustache')
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use('/public', express.static(path.join(__dirname, '/public')));
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache')
 
-app.get('/', function (req, res, next){
-     res.render('snippetdisplayindex')
+app.get('/', function(req, res, next) {
+  Snippet.find().then(function(mySnippets) {
+    res.render('snippetdisplayindex', {
+      mySnippets:mySnippets
+    })
+  })
 })
-app.post('/', function (req,res,next){
+app.post('/', function(req, res, next) {
   const title = req.body.title;
   const snip = req.body.snippet;
   const notes = req.body.notes;
   const language = req.body.language;
   let tagString = req.body.tags;
   const tags = tagString.split(' ');
-  const snippet = new Snippet({title:title, snippet:snip, notes:notes, language:language, tags:tags})
+  const snippet = new Snippet({
+    title: title,
+    snippet: snip,
+    notes: notes,
+    language: language,
+    tags: tags
+  })
   snippet.save()
-  .then( function(){
-    return Snippet.find();
-  }).then(function(mySnippets){
-    res.render('snippetdisplayindex',{mySnippets:mySnippets})
-  })
+    .then(function() {
+      return Snippet.find();
+    }).then(function(mySnippets) {
+      res.render('snippetdisplayindex', {
+        mySnippets: mySnippets
+      })
+    })
 
 
 })
-app.get('/:id', function (req,res,next){
+app.get('/:id', function(req, res, next) {
   let id = req.params.id;
-  Snippet.findOne({_id:new ObjectId()})
-  .then(function(snip){
-    res.render('updateSnippet', {onesnip:snip})
-  })
+  Snippet.findOne({
+      _id: new ObjectId(id)
+    })
+    .then(function(mySnippets) {
+      res.render('updateSnippet', {
+        oneSnip:mySnippets
+      })
+    })
 })
 
-app.post('/:id', function(req,res,next){
+app.post('/:id', function(req, res, next) {
   const uptitle = req.body.title;
   const upsnip = req.body.snippet;
   const upnotes = req.body.notes;
   const uplanguage = req.body.language;
   let tagString = req.body.tags;
   const uptags = tagString.split(' ');
-  Snippet.updateOne(_id: new ObjectId(id)},{title:uptitle, snippet:upsnip, notes:upnotes, language:uplanguage, tags:uptags})
-  .then(function(){
-    return Snippet.find();
-  }).then(function(mySnippets){
-    res.render('snippetdisplayindex',{mySnippets:mySnippets})
-  })
+  let id = req.params.id;
+  Snippet.updateOne({
+      _id: new ObjectId(id)
+    }, {
+      title: uptitle,
+      snippet: upsnip,
+      notes: upnotes,
+      language: uplanguage,
+      tags: uptags
+    })
+    .then(function() {
+      return Snippet.find();
+    }).then(function(mySnippets) {
+      res.render('snippetdisplayindex', {
+        mySnippets: mySnippets
+      })
+    })
 })
 
 app.listen(3000, function() {
-    console.log('successfully started Express Application');
-  })
+  console.log('successfully started Express Application');
+})
 
-  process.on('SIGINT', function() {
-    console.log("\nshutting down");
-    mongoose.connection.close(function () {
-      console.log('Mongoose default connection disconnected on app termination');
-      process.exit(0);
-    });
+process.on('SIGINT', function() {
+  console.log("\nshutting down");
+  mongoose.connection.close(function() {
+    console.log('Mongoose default connection disconnected on app termination');
+    process.exit(0);
   });
+});
